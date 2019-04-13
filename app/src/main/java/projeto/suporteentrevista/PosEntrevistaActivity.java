19bb15e8605e61c68c0e0f1186635e6a357999ad
JPedroRSilva <johnnysilva93@gmail.com>
2019-04-13 22:51:56 +0100

    xml

--- app/src/main/java/projeto/suporteentrevista/PosEntrevistaActivity.java
+++ app/src/main/java/projeto/suporteentrevista/PosEntrevistaActivity.java
@@ -1,4 +1,27 @@
 package projeto.suporteentrevista;
 
-public class PosEntrevistaActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class PosEntrevistaActivity extends AppCompatActivity {
+
+    /*****************Variaveis ****************/
+    private TextView resumoView;
+    private EditText notasView;
+    private ImageButton terminarBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        resumoView = (TextView) findViewById(R.id.resumoTextView);
+        notasView = (EditText) findViewById(R.id.notasEditText);
+        terminarBtn = (ImageButton) findViewById(R.id.terminarBtn);
+
+        preencheResumo()
+    }
 }
